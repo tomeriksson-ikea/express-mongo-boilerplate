2f4d600afe6d83e0dc76eb35711101fc54dd68fe
Tom Eriksson <tom.eriksson@ingka.ikea.com>
2024-05-22 10:33:05 +0200

    added changes for test

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -6,6 +6,6 @@ describe('GET /', () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello World!');
-
+        // hello
     });
 });
